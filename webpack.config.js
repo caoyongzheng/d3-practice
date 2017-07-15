e932719ed5c2435180000b5e4fd16b95f603d6c4
caoyongzheng <15267056106@139.com>
2017-07-15 20:03:30 +0800

    bar

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+const isProduction = process.env.NODE_ENV === 'production';
+const publicPath = isProduction ? '/d3-practice/' : '/dist/';
+const port = 9000;
+
+const config = {
+  entry: {
+    vendor: ['d3', 'd3-axis', 'd3-scale'],
+    bar: './src/bar.js',
+  },
+  output: {
+    filename: '[name]-[chunkhash:6].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, 'src'),
+        ],
+        exclude: [/node_modules/, path.resolve(__dirname, 'src', 'lib')],
+        loader: 'babel-loader',
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor-[chunkhash:8].js',
+      minChunks: Infinity,
+    }),
+  ],
+  devServer: {
+    host: '0.0.0.0',
+    contentBase: path.join(__dirname, 'dist'),
+    port,
+    compress: true,
+    publicPath,
+    disableHostCheck: true,
+    noInfo: true,
+  },
+};
+
+Object.keys(config.entry).filter(e => e !== 'vendor').forEach((e) => {
+  config.plugins.push(new HtmlWebpackPlugin({
+    chunks: ['vendor', e],
+    template: path.resolve(__dirname, 'src/template.html'),
+    filename: path.join(__dirname, 'dist', `${e}.html`),
+  }));
+  console.log(`http://0.0.0.0:9000/dist/${e}.html`);
+});
+
+if (isProduction) {
+  const CleanAssetsPlugin = new CleanWebpackPlugin(['dist'], {
+    root: path.resolve(__dirname),
+    verbose: true,
+    dry: false,
+  });
+  config.plugins.push(CleanAssetsPlugin);
+}
+
+module.exports = config;
